b1247f6d4cf28f296a9d210702c061560014eafa
knowledge91 <dirkhornung91@gmail.com>
2018-01-27 20:59:35 +0100

    malformed proxy response ( body needs to be JSON.stringified )

--- Lambda/Alda/lib/lambda.js
+++ Lambda/Alda/lib/lambda.js
@@ -7,13 +7,13 @@ const proxyResponse = (statusCode, headers, body) => {
 };
 
 export const respondOK = (callback) => {
-    console.log(proxyResponse(200, {"x-custom-header": "my custom header valye"}, {ok: "ok"}));
+    console.log(proxyResponse(200, {}, {}));
     console.log('responded OK');
     callback(null, proxyResponse(200, {}, {}));
 };
 
 export const respondError = (callback) => {
-    callback(null, proxyResponse(500, {"x-custom-header": "my custom header valye"}, {error: "true"}));
+    callback(null, proxyResponse(500, {}, {}));
 }
 
 
