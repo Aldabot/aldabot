fd6d9d393852f14fbdc8e1d7cc90312e8a6f9690
knowledge91 <dirkhornung91@gmail.com>
2018-01-29 23:00:25 +0100

    added several Categories

--- Lambda/Alda/lib/intents.js
+++ Lambda/Alda/lib/intents.js
@@ -48,32 +48,131 @@ const queryExpenses = (pool, psid) => {
     return retrieveTransactions(pool, psid).then((transactions) => {
         let total = 0;
         let expenses = {
+            "transport": {
+                name: "Transport",
+                emoji: "🚂",
+                amount: 0
+            },
+            "bills": {
+                name: "Bills",
+                emoji: "💸",
+                amount: 0
+            },
+            "education": {
+                name: "Education",
+                emoji: "💸",
+                amount: 0
+            },
+            "entertainment": {
+                name: "Entertainment",
+                emoji: "🎡",
+                amount: 0
+            },
+            "fitness": {
+                name: "Fitness",
+                emoji: "⚽",
+                amount: 0
+            },
             "shopping": {
                 name: "Shopping",
                 emoji: "🛍",
+                amount: 0
+            },
+            "food": {
+                name: "Food",
+                emoji: "🍏",
+                amount: 0
+            },
+            "health": {
+                name: "Health",
+                emoji: "🏥",
+                amount: 0
+            },
+            "kids": {
+                name: "Kids",
+                emoji: "🍼",
+                amount: 0
+            },
+            "pets": {
+                name: "Pets",
+                emoji: "🐶",
                 amount: 0
             },
             "restaurants": {
                 name: "Restaurants",
                 emoji: "🥘",
                 amount: 0
+            },
+            "travel": {
+                name: "Travel",
+                emoji: "✈️",
+                amount: 0
             }
         };
         var response = "";
         for (let transaction of transactions) {
             switch(transaction.category) {
-                case "shopping":
+            case "car_rental":
+            case "gas_and_fuel":
+            case "parking":
+            case "public_transportation":
+            case "service_and_parts":
+            case "taxi":
+                expenses.travel.amount -= transaction.amount;
+                break;
+            case "bills":
+                expenses.bills.amount -= transaction.amount;
+                break;
+            case "books_and_supplies":
+                expenses.eduation.amount -= transaction.amount;
+                break;
+            case "amusement":
+            case "games":
+            case "movies_and_music":
+            case "newspapers_and_magazines":
+                expenses.entertainment.amount -= transaction.amount;
+                break;
+            case "clothing":
+            case "electronics_and_software":
+            case "sporting_goods":
+            case "shopping":
                 expenses.shopping.amount -= transaction.amount;
                 break;
             case "cafes_and_restaurants":
             case "alcohol_and_bars" :
                 expenses.restaurants.amount -= transaction.amount;
                 break;
+            case "groceris":
+                expenses.food.amount -= transaction.amount;
+                break;
+            case "personal_car":
+            case "sports":
+                expenses.fitness.amount -= transactions.amount;
+                break;
+            case "doctor":
+            case "pharmacy":
+                expenses.health.amount -= transaction.amount;
+                break;
+            case "allowance":
+            case "baby_supplies":
+            case "child_support":
+            case "kids_activities":
+            case "toys":
+            case "babysitter_and_daycare":
+                expenses.kids.amount -= transaction.amount;
+                break;
+            case "pet_food_and_supplies":
+            case "pet_grooming":
+            case "veterinary":
+                expenses.pets.amount -= transaction.amount;
+                break;
+            case "hotel":
+            case "transportation":
+            case "vacation":
+                expenses.travel.amount -= transaction.amount;
             }
         }
         Object.keys(expenses).forEach((key) => {
-            console.log(key);
-            console.log(expenses[key]);
             if(expenses[key].amount != 0) {
                 response += expenses[key].amount.toFixed(0) + "€ " + translateToSp(expenses[key].name) + " " + expenses[key].emoji + "\n";
                 total += expenses[key].amount;
--- Lambda/Alda/lib/translate.js
+++ Lambda/Alda/lib/translate.js
@@ -1,16 +1,25 @@
 const spDictionary = {
     'account': 'Cuenta',
+    "Bills": "Facturas",
     'bonus': 'Bonificación',
     'card': 'Tarjeta',
     'checking': 'Cuenta',
     'credit_card': 'Tarjeta',
     'debit_card': 'Tarjeta',
+    'Education': "Educación",
     'ewallet': 'Billetera',
+    "Fitness": "Deportes",
+    "Food": "Alimentación",
     'insurance': 'Seguro',
     'investment': 'Inversión',
+    "Health": "Salud",
+    "Kids": "Niños",
     'mortgage': 'Hipoteca',
+    "Pets": "Mascotas",
+    "Restaurants": "Restaurantes",
     'savings': 'Ahorros',
     'Shopping': 'Compras',
+    "Travel": "Viajes",
     'loan': 'Préstamo'
 };
 
